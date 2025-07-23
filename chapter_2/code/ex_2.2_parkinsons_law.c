dfa44eddf216291d1d17467b1076b716f6ad5a44
Timo Füle <timo.fuele@gmail.com>
2025-07-23 16:53:54 +0200

    Solve Ex 2.2, parkinsons law, identify directives and statements.

---
+++ chapter_2/code/ex_2.2_parkinsons_law.c
@@ -0,0 +1,10 @@
+/*In this exercise directives and statements should be identified.*/
+#include <stdio.h> //includes the header for the standard input/output library
+
+int main(void) //int typ function "main" which takes no arguments (void)
+{ //opening the function body
+  printf("Parkinsons' Law:\nWork expands so as to "); //printing formatted string literal with extra new line in the middle and none at the end.
+  printf("fill the time\n"); //string literal with new line at the end
+  printf("available for its completion.\n"); //same as above
+  return 0; //exit the function returning error code 0, which means no error occurred
+} //closing the funcion body
