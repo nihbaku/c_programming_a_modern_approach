0f2b083c3deddbdc54f6531e879f55af20c1cddf
Timo Füle <timo.fuele@gmail.com>
2025-07-28 01:25:27 +0200

    Start exercise section 5.1

---
+++ chapter_5/code/ex_5.1_relations_and_if.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k;
+
+  i = 2; j = 3;
+  k = i * j == 6;
+  printf("Should be 1: %d\n", k);
+
+  i = 5; j = 10; k = 1;
+  printf("\n\nShould be 1: %d\n", k > i < j);
+
+  i = 3; j = 2; k = 1;
+  printf("\n\nShould be 1: %d\n", i < j == j < k);
+
+  i = 3; j = 4; k = 5;
+  printf("\n\nShould be 0: %d\n", i % j + i < k);
+}
