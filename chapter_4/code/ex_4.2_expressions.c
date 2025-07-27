395f2d5b11f7c26c64131a8f4228eb2de20b31b9
Timo Füle <timo.fuele@gmail.com>
2025-07-27 21:44:16 +0200

    Finish ex_4.2

--- chapter_4/code/ex_4.2_expressions.c
+++ chapter_4/code/ex_4.2_expressions.c
@@ -2,7 +2,28 @@
 
 int main(void)
 {
-  int i, j;
+  int i, j, k;
+
+  i = 7; j = 8;
+  i *= j + 1;
+  printf("Should be 64 8\n");
+  printf("%d %d\n", i, j);
+
+  i = j = k = 1;
+  i += j += k;
+  printf("\n\nShould be 3 2 1\n");
+  printf("%d %d %d\n", i, j, k);
+
+  i = 1; j = 2; k = 3;
+  i -= j -= k;
+  printf("\n\nShould be 2 -1 3\n");
+  printf("%d %d %d\n", i, j, k);
+
+  i = 2; j = 1; k = 0;
+  i *= j *= k;
+  printf("\n\nShould be 0 0 0\n");
+  printf("%d %d %d\n", i, j, k);
+
   i = 6;
   j = i += i;
   printf("\n\nShould be 12 12\n");
